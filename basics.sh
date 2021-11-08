0180b3cc90ad9e299c87e2c4e0e7baaeb628a318
macccorehenni <93945758+macccorehenni@users.noreply.github.com>
2021-11-08 14:28:19 -0800

    Create basics.sh

---
+++ basics.sh
@@ -0,0 +1,13 @@
+# nano— a command line text editor
+sudo apt install nano
+# Pale Moon— a Mozilla Firefox clone that works beautifully
+#Do note that Pale Moon, for some reason, doesn't create a Everything Launcher icon, so I do that manually 
+echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/home:stevenpusser.list
+curl -fsSL https://download.opensuse.org/repositories/home:stevenpusser/xUbuntu_18.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_stevenpusser.gpg > /dev/null
+sudo apt update
+sudo apt install palemoon
+touch /usr/share/applications/palemoon.desktop
+# Artha— a dictionary and thesaurus app
+sudo apt install Aretha
+#Kate— a graphical text editor
+sudo apt install Kate
