56257378a5c9ce0a3366ebc4d788010a3b7213fa
macccorehenni <93945758+macccorehenni@users.noreply.github.com>
2021-11-08 15:48:40 -0800

    Update basics.sh
    fixes

--- basics.sh
+++ basics.sh
@@ -6,8 +6,8 @@ echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser/xUbuntu_1
 curl -fsSL https://download.opensuse.org/repositories/home:stevenpusser/xUbuntu_18.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_stevenpusser.gpg > /dev/null
 sudo apt update
 sudo apt install palemoon
-touch /usr/share/applications/palemoon.desktop
+sudo touch /usr/share/applications/palemoon.desktop
 # Artha— a dictionary and thesaurus app
 sudo apt install artha
 #Kate— a graphical text editor
-sudo apt install Kate
+sudo apt install kate
